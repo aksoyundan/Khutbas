13230ab7f4b5b0cb72f55d3aa9f838dec2fd0ffc
Ozan Aksoy <aksoyundan@gmail.com>
2021-02-09 22:33:10 +0000

    Tweet scraping added

--- Scripts/Tweets.R
+++ Scripts/Tweets.R
@@ -1,27 +1,99 @@
 library(rtweet)
 library(tidyverse)
+library(lubridate)
 
-apn <- "Friday-khutbas"
-key <- "QPyPimCLYUBe4bT2PuQnw2era"
-sec <- "3zfDThCdF9BGu3Q5vt6BZYjCIeShZy838I6SAvZ62LgLSdWHad"
-tok <- "AAAAAAAAAAAAAAAAAAAAALpLMQEAAAAAVK6txwxShISW%2BJkVPxyjWqvt%2Fi0%3DYwgrpWgvZHPiKFZm3CBNS4RPn0Bsgsef1kWKWGSoZ1MdxZDFkz"
-
-# google API: AIzaSyBEzHbvHassrigxQJku5prG3TzdYEUbBiM
+source("api-keys.txt")
+source("Scripts/get_tweets.R")
 
 twitter_token <- create_token(
-  app = apn,
-  consumer_key = key,
-  consumer_secret = sec)
+  app = apn2,
+  consumer_key = key2,
+  consumer_secret = sec2)
 
 path_to_token <- file.path(path.expand("~"), ".twitter_token.rds")
 saveRDS(twitter_token, path_to_token)
-
 env_var <- paste0("TWITTER_PAT=", path_to_token)
 cat(env_var, file = file.path(path.expand("~"), ".Renviron"), 
     fill = TRUE, append = TRUE)
 readRenviron("~/.Renviron")
 
 
+kwords <- c("temiz", "sağlık", "salgın", "dikkat", 
+            "aile", "anne", "çocuk", "huzur", 
+            "millet", "şehit", "vatan", "fitne",
+            "tevhid", "ümmet", "zulüm", "müslüman",
+            "haram", "helal", "zarar", "mal",
+            "sabır", "yardım", "imtihan", "gayret",
+            "güven", "emin", "mübarek", "ahlak")
+
+kwords <- paste(kwords, collapse = " OR ")
+kwords <- paste("(", kwords, ")", sep="")
+kwords <- paste(kwords, "-is:retweet lang:tr place_country:TR")
+
+kw.nat <- c("millet", "şehit", "vatan", "fitne")
+kw.nat <- paste(kw.nat, collapse = " OR ")
+kw.nat <- paste("(", kw.nat, ")", sep="")
+kw.nat <- paste(kw.nat, " -is:retweet lang:tr place_country:TR -vekil")
+
+# "2021-02-08T00:00:00Z"
+
+
+
+#df <- get_tweets(kw.nat, 500, 
+#                 "2015-01-01T00:00:00Z",
+#                 "2016-01-08T00:00:00Z", tok2, next_token)
+
+#df.tidy <- as_tibble(df$data)
+#df.tidy2 <- as_tibble(df$includes$users)
+
+#paginate tweets and download
+
+start_date0 <- "2015-01-01T00:00:01Z" #start date of downloading tweets
+int0 <- ((ymd("2015-01-01")%--%ymd(Sys.Date())) /years(1))/300 #time between start date and today in years
+end_date0 <- sub(" ","T", paste0(lubridate::ymd_hms(start_date0) + lubridate::dyears(int0), "Z"))
+next_token <-""
+k <- 0
+
+while(k<15*60){
+  int <- int0*(k/3)
+  start_date <- sub(" ","T", paste0(lubridate::ymd_hms(start_date0) + lubridate::dyears(int), "Z"))
+  end_date   <- sub(" ","T", paste0(lubridate::ymd_hms(end_date0) + lubridate::dyears(int), "Z"))
+  
+  df <- get_tweets(kwords, 500, 
+                   start_date,
+                   end_date, tok2, next_token)
+  
+  jsonlite::write_json(df$data,paste0("data/","data_",df$data$id[nrow(df$data)],".json"))
+  jsonlite::write_json(df$includes,paste0("data/","includes_",df$data$id[nrow(df$data)],".json"))
+  next_token <- df$meta$next_token #this is NULL if there are no pages left
+  Sys.sleep(3.1)
+  k <- k+3
+  cat(k,": ","(",nrow(df$data),") ",df$data$created_at[nrow(df$data)],"\n",sep = "")
+}
+
+
+
+### merge downloaded tweets
+
+files <- list.files(path = "data/", pattern = "^data_")
+data_file <- file.path("data", files)
+
+
+tws <- data.frame("created_at" = c(), "author_id" = c(),"id" = c(), "text" = c())
+
+for (i in 1:length(data_file)) {
+  data.frame("created_at" = as_tibble(jsonlite::fromJSON(data_file[i]))$created_at , 
+             "author_id" = as_tibble(jsonlite::fromJSON(data_file[i]))$author_id , 
+             "id" = as_tibble(jsonlite::fromJSON(data_file[i]))$id , 
+             "text" = as_tibble(jsonlite::fromJSON(data_file[i])$text) ) -> doc
+  colnames(doc) <- c("created_at", "author_id", "id", "text")
+  tws  <- rbind(tws,doc) 
+}
+
+saveRDS(tws, "data/tws.RDA")
+################
+
+# "T00:00:00Z"
 
 library(lubridate)
 library(ggplot2)
@@ -49,6 +121,23 @@ kwords <- c("temiz", "sağlık", "salgın", "dikkat",
             "güven", "emin", "mübarek", "ahlak")
 
 kwords <- paste(kwords, collapse = " OR ")
+
+kw.nat <- c("millet", "şehit", "vatan", "fitne")
+kw.nat <- paste(kw.nat, collapse = " OR ")
+
+
+next_token <-""
+k <- 0
+while(k<15*60){
+  df <- get_tweets(q,n,start_time,end_time,bearer,next_token)
+  jsonlite::write_json(df$data,paste0("data/","data_",df$data$id[nrow(df$data)],".json"))
+  jsonlite::write_json(df$includes,paste0("data/","includes_",df$data$id[nrow(df$data)],".json"))
+  next_token <- df$meta$next_token #this is NULL if there are no pages left
+  Sys.sleep(3.1)
+  k <- k+3
+  cat(k,": ","(",nrow(df$data),") ",df$data$created_at[nrow(df$data)],"\n",sep = "")
+}
+
 
 tws <- search_tweets(kwords, lang = "tr", n = 18000, retryonratelimit = TRUE,
                      include_rts = FALSE, geocode = lookup_coords("Turkey"))
